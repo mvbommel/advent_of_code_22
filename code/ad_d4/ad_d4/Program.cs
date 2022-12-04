53a0e8d9bc7d4b41d51cfe4b69b1d62bbe669373
marc van bommel <mijngamemail@ziggo.nl>
2022-12-04 10:30:31 +0100

    day4 part 1

---
+++ code/ad_d4/ad_d4/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+string text = File.ReadAllText(@"G:\marc\advent_of_code\advent_of_code_22\txt\day4.txt");
+
+string[] pairs = text.Split("\r\n");
+int total = 0;
+
+foreach (string pair in pairs)
+{
+    string[] areas = pair.Split(',');
+    int[][] both = new int[areas.Length][];
+    for (int i = 0; i < areas.Length; i++)
+    {
+        string[] textEnds = areas[i].Split("-");
+        int[] ends = new int[2];
+        int count = 0;
+        foreach (string te in textEnds)
+        {
+            ends[count] = Int32.Parse(te);
+            count++;
+        }
+        both[i] = ends;
+    }
+
+    if (both[0][0] <= both[1][0] && both[0][1] >= both[1][1])
+    {
+        total++;
+    }
+    else if (both[1][0] <= both[0][0] && both[1][1] >= both[0][1])
+    {
+        total++;
+    }
+}
+Console.WriteLine(total);
+
+
+
