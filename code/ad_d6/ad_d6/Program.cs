de530cec76c43d3db0188c3013e7cf6e60d54e20
marc van bommel <mijngamemail@ziggo.nl>
2022-12-06 10:59:23 +0100

    day 6 part 1

---
+++ code/ad_d6/ad_d6/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+string text = File.ReadAllText(@"G:\marc\advent_of_code\advent_of_code_22\txt\day6.txt");
+char[] characters = text.ToCharArray();
+
+List<char> marker = new List<char>();
+int startIndex = 4;
+foreach( char c in characters)
+{
+    if(marker.Count < 4)
+    {
+        marker.Add(c);
+    }
+    else
+    {
+        if (marker.Count() != marker.Distinct().Count())
+        {
+            marker.RemoveAt(0);
+            startIndex++;
+            marker.Add(c);
+        }
+        else
+        {
+            break;
+        }
+        
+    }
+}
+Console.WriteLine(startIndex);
