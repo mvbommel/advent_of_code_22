25b2446ea45ccfad82d20f141313a636689fca93
marc van bommel <mijngamemail@ziggo.nl>
2022-12-12 10:43:14 +0100

    attempt for day8 part 1

---
+++ code/ad_d8/ad_d8/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Drawing;
+
+List<string> lines = File.ReadAllLines(@"..\..\..\..\..\..\txt\day8.txt").ToList();
+
+HashSet<Point> visible = new HashSet<Point>();
+for (int row = 0; row < lines.Count; row++)
+{
+    string s2 = lines[row];
+    char highest = '0';
+    char reverseHighest = '0';
+    for (int col =0; col < s2.Length; col++)
+    {
+        
+        if (s2[col] > highest)
+        {
+            visible.Add( new Point(row, col));
+            highest= s2[col];
+        }
+        int rCol = s2.Length - (col + 1);
+        if (s2[rCol] > reverseHighest)
+        {
+            visible.Add(new Point(row, rCol));
+            reverseHighest = s2[rCol];
+        }
+    }
+
+}
+string s = lines[0];
+for (int col = 0; col < s.Length; col++)
+{
+    char highestDown = '0';
+    char highestUp = '0';
+    for (int row = 0; row < lines.Count; row++)
+    {
+        string s2 = lines[row];
+        if (s2[col] > highestDown)
+        {
+            visible.Add(new Point(row, col));
+            highestDown = s2[col];
+        }
+        int rowT = lines.Count - (row + 1);
+        s2 = lines[rowT];
+        if (s2[col] > highestUp)
+        {
+            visible.Add(new Point(rowT, col));
+            highestUp = s2[col];
+        }
+    }
+
+}
+
+Console.WriteLine(visible.Count);
