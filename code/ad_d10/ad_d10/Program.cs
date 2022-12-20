22d856e9559578a1f25d816947aec35cb5758760
marc van bommel <mijngamemail@ziggo.nl>
2022-12-20 17:29:06 +0100

    day 10 part 2

--- code/ad_d10/ad_d10/Program.cs
+++ code/ad_d10/ad_d10/Program.cs
@@ -4,28 +4,38 @@ List<string> lines = File.ReadAllLines(@"..\..\..\..\..\..\txt\day10.txt").ToLis
 List<int> signalStrenghts = new List<int>();
 int currentTick = 0;
 int x = 1;
+string rOne = "";
+string rTwo = "";
+string rThree = "";
+string rFour = "";
+string rFive = "";
+string rSix = "";
+
 foreach (string line in lines)
 {
     if (line.Contains("noop"))
     {
         currentTick++;
-        if ( findSignalStrength(currentTick) != 0)
+        draw();
+        if ( findSignalStrength() != 0)
         {
-            signalStrenghts.Add(findSignalStrength(currentTick));
+            signalStrenghts.Add(findSignalStrength());
         }
         
     }
     else if (line.Contains("addx"))
     {
         currentTick++;
-        if (findSignalStrength(currentTick) != 0)
+        draw();
+        if (findSignalStrength() != 0)
         {
-            signalStrenghts.Add(findSignalStrength(currentTick));
+            signalStrenghts.Add(findSignalStrength());
         }
         currentTick++;
-        if (findSignalStrength(currentTick) != 0)
+        draw();
+        if (findSignalStrength() != 0)
         {
-            signalStrenghts.Add(findSignalStrength(currentTick));
+            signalStrenghts.Add(findSignalStrength());
         }
         string[] addValues = line.Split(' ');
         x += Int32.Parse(addValues[1]);
@@ -39,7 +49,14 @@ foreach (int strength in signalStrenghts)
     total += strength;
 }
 Console.WriteLine(total);
-int findSignalStrength(int currentTick)
+Console.WriteLine(rOne);
+Console.WriteLine(rTwo);
+Console.WriteLine(rThree);
+Console.WriteLine(rFour);
+Console.WriteLine(rFive);
+Console.WriteLine(rSix);
+
+int findSignalStrength()
 {
     switch (currentTick)
     {
@@ -57,6 +74,72 @@ int findSignalStrength(int currentTick)
             return x * 220;
          default: return 0;
     }
-        
+}
 
+void draw()
+{
+    Console.WriteLine("currenttick " + currentTick + " x " + x);
+    switch(currentTick)
+    {
+        case var expression when currentTick <= 40:
+            if(currentTick == x || currentTick == x+1 || currentTick == x + 2)
+            {
+                rOne += 'X';
+            }
+            else
+            {
+                rOne +=' ';
+            }
+            break;
+        case var expression when currentTick > 40 && currentTick <= 80:
+            if (currentTick - 40 == x || currentTick -40 == x + 1 || currentTick -40 == x + 2)
+            {
+                rTwo += 'X';
+            }
+            else
+            {
+                rTwo += ' ';
+            }
+            break;
+        case var expression when currentTick > 80 && currentTick <= 120:
+            if (currentTick -80  == x || currentTick -80 == x + 1 || currentTick -80 == x + 2)
+            {
+                rThree += 'X';
+            }
+            else
+            {
+                rThree += ' ';
+            }
+            break;
+        case var expression when currentTick > 120 && currentTick <= 160:
+            if (currentTick -120 == x || currentTick -120 == x + 1 || currentTick - 120 == x + 2)
+            {
+                rFour += 'X';
+            }
+            else
+            {
+                rFour += ' ';
+            }
+            break;
+        case var expression when currentTick > 160 && currentTick <= 200:
+            if (currentTick - 160 == x || currentTick - 160 == x + 1 || currentTick -160 == x + 2)
+            {
+                rFive += 'X';
+            }
+            else
+            {
+                rFive += ' ';
+            }
+            break;
+        case var expression when currentTick > 200 && currentTick <= 240:
+            if (currentTick - 200 == x || currentTick -200 == x + 1 || currentTick - 200 == x + 2)
+            {
+                rSix += 'X';
+            }
+            else
+            {
+                rSix += ' ';
+            }
+            break;
+    }
 }
